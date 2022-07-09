c47779f11583191fb9afd1b7631ac575cc0297dd
slimforge9 <slimforge9@gmail.com>
2022-07-09 18:40:38 +0200

    API 2nd Call

--- python_repos.py
+++ python_repos.py
@@ -5,8 +5,19 @@ url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 headers = {'Accept': 'application/vnd.github.v3_+json'}
 r = requests.get(url, headers=headers)
 print(f"Kod stanu: {r.status_code}")
+
 # Umieszczenie odpowiedzi API w zmiennej.
 response_dict = r.json()
+print(f"Całkowita liczba repozytoriów: {response_dict['total_count']}")
 
-# Przetworzenie wyników
-print(response_dict.keys())
+# Przetworzenie informacji o  repozytoriach.
+repo_dicts = response_dict['items']
+print(f"Liczba zwróconych repozytoriów: {len(repo_dicts)}")
+
+print("\nWybrane informacje o każdym repozytorium.")
+for repo_dict in repo_dicts:
+    print(f"Nazwa: {repo_dict['name']}")
+    print(f"Właściciel: {repo_dict['owner']['login']}")
+    print(f"Gwiazdki: {repo_dict['stargazers_count']}")
+    print(f"Repozytorium: {repo_dict['html_url']}")
+    print(f"Opis: {repo_dict['description']}\n")
---
+++ python_repos_visual.py
@@ -0,0 +1,55 @@
+import requests
+
+from plotly.graph_objs import Bar
+from plotly import offline
+
+
+# Wykonanie wywołania API i zachowanie otrzymanej odpowiedzi.
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers = {'Accept': 'application/vnd.github.v3_+json'}
+r = requests.get(url, headers=headers)
+print(f"Kod stanu: {r.status_code}")
+
+# Przetworzenie wyników
+response_dict = r.json()
+repo_dicts = response_dict['items']
+repo_names, stars, labels = [], [], []
+for repo_dict in repo_dicts:
+    repo_names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+    owner = repo_dict['owner']['login']
+    description = repo_dict['description']
+    label = f"{owner}<br />{description}"
+    labels.append(label)
+
+# Utworzenie wiuzualizacji.
+data = [{
+    'type': 'bar',
+    'x': repo_names,
+    'y': stars,
+    'hovertext': labels,
+    'marker': {
+        'color': 'rgb(60, 100, 150)',
+        'line': {'width': 1.5, 'color': 'rgb(25, 25, 25)'}
+    },
+    'opacity': 0.6,
+}]
+
+my_layout = {
+    'title': 'Oznaczone największą liczbą gwiazdek projekty Pythona w serwisie Github',
+    'titlefont': {'size': 28},
+    'xaxis': {
+        'title': 'Repozytorium',
+        'titlefont': {'size': 24},
+        'tickfont': {'size': 14}
+    },
+    'yaxis': {
+        'title': 'Gwiazdki',
+        'titlefont': {'size': 24},
+        'tickfont': {'size': 14},
+    },
+}
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='python_repos.html')
